10fc08986f086338738891692eed9b0173021163
VUMotuzok <m93vu@ya.ru>
2022-11-16 22:20:09 +0400

    Basic createFundsReserveHandler

--- handlers.go
+++ handlers.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"database/sql"
 	"github.com/go-playground/validator/v10"
 	"github.com/gofiber/fiber/v2"
 	"github.com/google/uuid"
@@ -17,7 +18,10 @@ type CreateFundsCreditHandlerDTO struct {
 }
 
 type CreateFundsReserveHandlerDTO struct {
-	UserId string `json:"userId" validate:"required,uuid"`
+	UserId    string `json:"userId" validate:"required,uuid"`
+	ServiceId string `json:"serviceId" validate:"required,uuid"`
+	OrderId   string `json:"orderId" validate:"required,uuid"`
+	Amount    int    `json:"amount" validate:"required,gte=0"`
 }
 
 type ApproveFundsReserveHandlerDTO struct {
@@ -30,31 +34,43 @@ func handleError(c *fiber.Ctx, err error) error {
 	})
 }
 
+func findUser(db *bun.DB, id string, c *fiber.Ctx) (*models.UserModel, bool, error) {
+	user := new(models.UserModel)
+
+	count, err := db.NewSelect().
+		Model(user).
+		Where("id=?", id).
+		ScanAndCount(c.Context())
+
+	if err != nil {
+		return user, false, err
+	}
+
+	if count == 0 {
+		return user, false, err
+	}
+
+	return user, true, err
+
+}
+
 func createFundsCreditHandler(c *fiber.Ctx) error {
 	db := createConnection()
-
 	dto := new(CreateFundsCreditHandlerDTO)
 
 	if err := c.BodyParser(dto); err != nil {
 		return handleError(c, err)
 	}
-
 	if err := validate.Struct(dto); err != nil {
 		return handleError(c, err)
 	}
 
-	user := new(models.UserModel)
-
-	exists, err := db.NewSelect().
-		Model(user).
-		Where("? = ?", bun.Ident("id"), dto.UserId).
-		Exists(c.Context())
-
+	user, isFound, err := findUser(db, dto.UserId, c)
 	if err != nil {
 		return handleError(c, err)
 	}
 
-	if !exists {
+	if !isFound {
 		_, err = db.NewInsert().Model(&models.UserModel{
 			Id:     uuid.Must(uuid.Parse(dto.UserId)),
 			Amount: dto.Amount,
@@ -65,15 +81,6 @@ func createFundsCreditHandler(c *fiber.Ctx) error {
 		}
 
 		return c.SendStatus(fiber.StatusCreated)
-	}
-
-	err = db.NewSelect().
-		Model(user).
-		Where("? = ?", bun.Ident("id"), dto.UserId).
-		Scan(c.Context())
-
-	if err != nil {
-		return handleError(c, err)
 	}
 
 	_, err = db.NewUpdate().
@@ -90,21 +97,59 @@ func createFundsCreditHandler(c *fiber.Ctx) error {
 }
 
 func createFundsReserveHandler(c *fiber.Ctx) error {
-	connection := createConnection()
-	log.Print(connection)
-
+	db := createConnection()
 	dto := new(CreateFundsReserveHandlerDTO)
 
 	if err := c.BodyParser(dto); err != nil {
 		return handleError(c, err)
 	}
 
-	err := validate.Struct(dto)
+	if err := validate.Struct(dto); err != nil {
+		return handleError(c, err)
+	}
+
+	user, isFound, err := findUser(db, dto.UserId, c)
 	if err != nil {
 		return handleError(c, err)
 	}
 
-	return c.SendString(dto.UserId)
+	if !isFound || user.Amount < dto.Amount {
+		return c.SendStatus(fiber.StatusBadRequest)
+	}
+
+	tx, err := db.BeginTx(c.Context(), &sql.TxOptions{})
+
+	_, err = tx.NewInsert().Model(&models.Transaction{
+		Amount:    dto.Amount,
+		OrderId:   uuid.Must(uuid.Parse(dto.OrderId)),
+		ServiceId: uuid.Must(uuid.Parse(dto.ServiceId)),
+		Status:    "reserved",
+		UserId:    uuid.Must(uuid.Parse(dto.UserId)),
+	}).Exec(c.Context())
+
+	if err != nil {
+		err = tx.Rollback()
+		return handleError(c, err)
+	}
+
+	_, err = db.NewUpdate().
+		Model(user).
+		Where("? = ?", bun.Ident("id"), dto.UserId).
+		Set("amount = ?", user.Amount-dto.Amount).
+		Exec(c.Context())
+
+	if err != nil {
+		err = tx.Rollback()
+		return handleError(c, err)
+	}
+
+	err = tx.Commit()
+
+	if err != nil {
+		return handleError(c, err)
+	}
+
+	return c.SendStatus(fiber.StatusOK)
 }
 
 func approveFundsReserveHandler(c *fiber.Ctx) error {
@@ -129,5 +174,5 @@ func getFundsHandler(c *fiber.Ctx) error {
 	connection := createConnection()
 	log.Print(connection)
 
-	return c.SendString("Hello")
+	return c.SendStatus(fiber.StatusOK)
 }
--- models/models.go
+++ models/models.go
@@ -11,3 +11,14 @@ type UserModel struct {
 	Id     uuid.UUID `bun:"id,pk,type:uuid"`
 	Amount int       `bun:"amount,notnull"`
 }
+
+type Transaction struct {
+	bun.BaseModel `bun:"table:transaction"`
+
+	Id        uuid.UUID `bun:"id,pk,type:uuid,default:uuid_generate_v4()"`
+	Amount    int       `bun:"amount,notnull"`
+	OrderId   uuid.UUID `bun:"order_id,notnull,type:uuid"`
+	ServiceId uuid.UUID `bun:"service_id,notnull,type:uuid"`
+	Status    string    `bun:"status,notnull,type:transaction_status"`
+	UserId    uuid.UUID `bun:"user_id,notnull,type:uuid"`
+}
